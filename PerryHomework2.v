b98ff807723a37a4e06a7be89d9b8829bc2d030a
Paul Kline <pauliankline@gmail.com>
2015-11-03 14:09:09 -0600

    more problem 2. finished? problem 1

--- PerryHomework2.v
+++ PerryHomework2.v
@@ -12,12 +12,8 @@ Inductive NatStack : Type :=
 
 Module problem1.
 
-
 Definition push(n:nat) (ns : NatStack) : NatStack :=
    Add n ns.
-
-
-
 
 Definition pop(ns : NatStack) : NatStack :=
    match ns with
@@ -25,24 +21,17 @@ Definition pop(ns : NatStack) : NatStack :=
     | Add _ ns' => ns' 
    end.
 
-
-
 Definition top (ns:NatStack):SC :=
    match ns with 
     | Empty => Unknown
     | Add x _ => Value x
    end. 
 
-
-
-
 Definition isEmpty(ns : NatStack) : bool :=
    match ns with 
     | Empty => true
     | Add _ _ => false
    end.
-
-
 
 
 (* push 
@@ -81,12 +70,6 @@ POSTCONDITIONS:
 
   INVARIANTS:
      1. ns is unchanged. 
-*)(*POSTCONDITIONS:
-     1.  empty <==> true 
-     2.  push n ns <==> false
-
-  INVARIANTS:
-     1. ns is unchanged.
 *)
 
 
@@ -124,8 +107,59 @@ Theorem popPost2 : forall ns:NatStack, forall n:nat, top ns = Value n <-> Value 
 Theorem popINVARIANT1: forall n : nat, forall ns : NatStack, pop (push n ns) = ns.
  Proof. apply pushINVARIANT1. Qed.
 
+Theorem topPOST1 : forall ns:NatStack, forall n :nat, top (push n ns) = Value n .
+ Proof. intros. simpl. reflexivity. Qed.
+Theorem topINVARIANT1 : forall ns:NatStack, forall n : nat, (top (push n ns)) = Value n -> ns = ns.
+ Proof. intros. reflexivity. Qed. 
+
+Definition s1 := push 1 (push 2 (push 3 Empty)).
+Definition emp := Empty .
+
+Eval compute in pop ( s1).
+Eval compute in pop (pop s1).
+Eval compute in pop (pop (pop s1)).
+Eval compute in pop (pop (pop (pop s1))).
 
 
+End problem1. 
+
+
+Module problem2. 
+
+Inductive SC : Type :=
+| Value : nat -> SC
+| Unknown : SC.
+
+Inductive NatStack : Type :=
+| Empty : NatStack
+| Add : nat -> NatStack -> NatStack.
+
+
+Module problem1.
+
+Definition push(sc:SC) (ns : NatStack) : NatStack :=
+   match sc with 
+    | Unknown => ns 
+    | Value n => Add n ns
+   end.
+
+Definition pop(ns : NatStack) : NatStack :=
+   match ns with
+    | Empty => Empty 
+    | Add _ ns' => ns' 
+   end.
+
+Definition top (ns:NatStack):SC :=
+   match ns with 
+    | Empty => Unknown
+    | Add x _ => Value x
+   end. 
+
+Definition isEmpty(ns : NatStack) : bool :=
+   match ns with 
+    | Empty => true
+    | Add _ _ => false
+   end.
 
 
 
